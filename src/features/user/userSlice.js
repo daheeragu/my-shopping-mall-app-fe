cda759ab6e4b57081259a309da9e3abf044fedda
Dahee Park <dahee5654@gmail.com>
2024-10-26 15:26:34 +0900

    modified Toast message and error handling

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,12 +1,27 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-
 import { showToastMessage } from "../common/uiSlice";
 import api from "../../utils/api";
 import { initialCart } from "../cart/cartSlice";
 
+//이메일로 로그인
 export const loginWithEmail = createAsyncThunk(
   "user/loginWithEmail",
-  async ({ email, password }, { rejectWithValue }) => {}
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const response = await api.post("/auth/login", { email, password });
+      //성공
+      // 홈으로 리다이렉트 로그인 페이지에서 처리
+      const data = response.data;
+      sessionStorage.setItem("token", data.token);
+      // 토큰값을 헤더에 넣음
+      //api.defaults.headers["authorization"] = "Bearer " + data.token;
+      return data;
+    } catch (error) {
+      //실패
+      // 실패시 생긴 에러값을 reducer에 저장
+      return rejectWithValue(error.message);
+    }
+  }
 );
 
 export const loginWithGoogle = createAsyncThunk(
@@ -28,7 +43,7 @@ export const registerUser = createAsyncThunk(
       // 1. 성공 토스트 메세지 보여주기
       dispatch(
         showToastMessage({
-          massage: "회원 가입을 성공했습니다!",
+          message: "회원 가입을 성공했습니다!",
           status: "success",
         })
       );
@@ -45,14 +60,21 @@ export const registerUser = createAsyncThunk(
         })
       );
       // 2. 에러 값을 저장한다
-      return rejectWithValue(error.massage);
+      return rejectWithValue(error.message);
     }
   }
 );
-
+// 토큰으로 로그인
 export const loginWithToken = createAsyncThunk(
   "user/loginWithToken",
-  async (_, { rejectWithValue }) => {}
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await api.get("/user/me");
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
 );
 
 const userSlice = createSlice({
@@ -81,6 +103,21 @@ const userSlice = createSlice({
       })
       .addCase(registerUser.rejected, (state, action) => {
         state.registrationError = action.payload;
+      })
+      .addCase(loginWithEmail.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(loginWithEmail.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.user;
+        state.loginError = null;
+      })
+      .addCase(loginWithEmail.rejected, (state, action) => {
+        state.loading = false;
+        state.loginError = action.payload;
+      })
+      .addCase(loginWithToken.fulfilled, (state, action) => {
+        state.user = action.payload.data.user;
       });
   },
 });
